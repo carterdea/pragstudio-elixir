c3aa974a82d63f071df1ada1f2654afaf116edaa
Carter De Angelis <carterdea@gmail.com>
2017-08-27 01:42:47 -0700

    Formatting, logging, etc

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -2,8 +2,10 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
   end
 
@@ -14,49 +16,120 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{ method: method, path: path, response_body: "" }
+    %{
+      method: method,
+      path: path,
+      response_body: "",
+      status: nil
+    }
   end
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(conv), do: conv
 
   def log(conv), do: IO.inspect conv
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+      %{ conv | status: 200, response_body: "Bears, Lions, Tigers, iPhones, iPads, Apple Watches" }
   end
 
-  def route(conv, "GET", "/wildthings") do
-      %{ conv | response_body: "Bears, Lions, Tigers, iPhones, iPads, Apple Watches" }
+  def route(%{ method: "GET", path: "/bears" } = conv) do
+    %{ conv | status: 200, response_body: "Grizzley, Teddy, Smokey, Paddington, Bear from the Revenant" }
   end
 
-  def route(conv, "GET", "/bears") do
-    %{ conv | response_body: "Grizzley, Teddy, Smokey, Paddington, Bear from the Revenant" }
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+    %{ conv | status: 200, response_body: "Bear #{id}" }
   end
+
+  def route(%{ path: path } = conv) do
+    %{ conv | status: 404, response_body: "No #{path} here. :(" }
+  end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning: #{path} is on the loose! >:]\n"
+    conv
+  end
+
+  def track(conv), do: conv
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
-    COntent-Length: #{byte_size(conv.response_body)}
+    Content-Length: #{String.length(conv.response_body)}
 
     #{conv.response_body}
     """
   end
+
+  # defp == private in Ruby
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+
 end
 
-# request = """
-# GET /wildthings HTTP/1.1
-# Host: example.com
-# User-Agent: ExampleBrowser/1.0
-# Accept: */*
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
 
-# """
+"""
 
-# request = """
-# GET /bears HTTP/1.1
-# Host: example.com
-# User-Agent: ExampleBrowser/1.0
-# Accept: */*
+response = Servy.Handler.handle(request)
 
-# """
+IO.puts response
+
+
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
 
 request = """
 GET /bigfoot HTTP/1.1
